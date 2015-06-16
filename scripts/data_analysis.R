f4c47e6e2cdc2b5e16766545b65ed4aa3e20d123
Hannah Wickenden <hannah.wickenden@hotmail.com>
2015-06-16 16:08:43 +0100

    Edit axes labels

--- scripts/data_analysis.R
+++ scripts/data_analysis.R
@@ -29,9 +29,9 @@ countries <- as.vector(unique(data$country))
 
 for (country in countries){
   nu <- calcGDP(data, country = country)
-  plot(nu$year, nu$pop, main = paste(country))
-  plot(nu$year, nu$lifeExp, main = paste(country))
-  plot(nu$year, nu$gdp, main = paste(country))
+  plot(nu$year, nu$pop, main = paste(country), xlab = paste(unique(nu$year)), ylab = paste(unique(nu$pop)))
+  plot(nu$year, nu$lifeExp, main = paste(country), xlab = paste(unique(nu$year)), ylab = paste(unique(nu$lifeExp)))
+  plot(nu$year, nu$gdp, main = paste(country), xlab = paste(unique(nu$year)), ylab = paste(unique(nu$gdp)))
 }
 }
 plotGDP(gapminder)
