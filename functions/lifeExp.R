83c72d0b52be103a9ab888059242c374a5dacaaa
Hannah Wickenden <hannah.wickenden@hotmail.com>
2015-06-16 17:09:22 +0100

    Renamed.

--- functions/lifeExp.R
+++
@@ -1,9 +0,0 @@
-# Calculate life expectency by given variables
-
-lifeExp <- function(data, variables){
-  df <- ddply(
-    .data = data,
-    .variables = variables,
-    .fun = function(x){mean(x$lifeExp)})
-  rename(df, c("V1" = "lifeExp"))
-}
